3f525e1ad1e12e5b8cd7763aec48e75a4a174ea6
huangjie <jie.huang@imike.com>
2015-10-12 22:57:24 +0800

    fix

---
+++ taskfactory-web/src/main/java/com/mk/taskfactory/web/controller/UploadRoomListController.java
@@ -0,0 +1,55 @@
+package com.mk.taskfactory.web.controller;
+
+
+import com.dianping.cat.Cat;
+import com.mk.taskfactory.api.OnSaleFallbackService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpSession;
+import java.io.File;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class UploadRoomListController {
+
+    @Autowired
+    private OnSaleFallbackService onSaleFallbackService;
+
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<Map<String, Object>> test(HttpSession httpSession) {
+        HashMap<String, Object> result = new HashMap<String, Object>();
+        result.put("check", "123");
+        Cat.logEvent("CHECK_TEST", "123");
+//        validRateTaskService.validRateTaskRun();
+        return new ResponseEntity<Map<String, Object>>(result, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/import", method = RequestMethod.POST)
+    public void importBrandSort(MultipartFile file) throws Exception {
+
+        if (file == null) {
+            return;
+        }
+
+        String name = file.getOriginalFilename();// 获取上传文件名,包括路径
+        long size = file.getSize();
+        if ((name == null || name.equals("")) && size == 0) {
+            return;
+        }
+
+        InputStream in = file.getInputStream();
+
+        return;
+    }
+
+}
