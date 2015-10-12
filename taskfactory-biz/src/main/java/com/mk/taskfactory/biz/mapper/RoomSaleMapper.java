9d6d95bf5faf1656f478613e1788c847369c4053
huangjie <jie.huang@imike.com>
2015-10-12 14:04:43 +0800

    add room sale

---
+++ taskfactory-biz/src/main/java/com/mk/taskfactory/biz/mapper/RoomSaleMapper.java
@@ -0,0 +1,11 @@
+package com.mk.taskfactory.biz.mapper;
+
+
+import com.mk.taskfactory.biz.repository.MyBatisRepository;
+
+import java.util.List;
+
+@MyBatisRepository
+public interface RoomSaleMapper {
+
+}
