30a16df5a775ca133e027c4e05af1b8abde45388
huangjie <jie.huang@imike.com>
2015-10-17 22:26:28 +0800

    add baseprice

---
+++ taskfactory-api/src/main/java/com/mk/taskfactory/api/BasePriceService.java
@@ -0,0 +1,10 @@
+package com.mk.taskfactory.api;
+
+/**
+ * Created by admin on 2015/10/17.
+ */
+public class BasePriceService {
+//    public TBaseprice selectByPrimaryKey(Long id);
+//
+//    public TBaseprice findByRoomtypeId(Long roomTypeId);
+}
