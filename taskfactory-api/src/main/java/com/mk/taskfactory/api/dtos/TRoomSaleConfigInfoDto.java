d682e24fb9ded81a17985766380e745b29bb4954
xiaolong.kang <xiaolong.kang@imike.com>
2015-10-19 21:51:41 +0800

    change shiti

---
+++ taskfactory-api/src/main/java/com/mk/taskfactory/api/dtos/TRoomSaleConfigInfoDto.java
@@ -0,0 +1,139 @@
+package com.mk.taskfactory.api.dtos;
+
+public class TRoomSaleConfigInfoDto {
+    private Integer id;
+    private Integer saleTypeId;
+    private String startDate;
+    private String endDate;
+    private String startTime;
+    private String endTime;
+    private String saleValue;
+    private String saleLabel;
+    private String description;
+    private String fontColor;
+    private String valid;
+    private String createTime;
+    private String createBy;
+    private String updateTime;
+    private String updateBy;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getSaleTypeId() {
+        return saleTypeId;
+    }
+
+    public void setSaleTypeId(Integer saleTypeId) {
+        this.saleTypeId = saleTypeId;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getSaleValue() {
+        return saleValue;
+    }
+
+    public void setSaleValue(String saleValue) {
+        this.saleValue = saleValue;
+    }
+
+    public String getSaleLabel() {
+        return saleLabel;
+    }
+
+    public void setSaleLabel(String saleLabel) {
+        this.saleLabel = saleLabel;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getFontColor() {
+        return fontColor;
+    }
+
+    public void setFontColor(String fontColor) {
+        this.fontColor = fontColor;
+    }
+
+    public String getValid() {
+        return valid;
+    }
+
+    public void setValid(String valid) {
+        this.valid = valid;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(String createBy) {
+        this.createBy = createBy;
+    }
+
+    public String getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(String updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getUpdateBy() {
+        return updateBy;
+    }
+
+    public void setUpdateBy(String updateBy) {
+        this.updateBy = updateBy;
+    }
+}
