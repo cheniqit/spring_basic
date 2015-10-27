bd5ed1844e1cd88ddf449fb30fea9625de9d5082
kirin.li <kirin.li@dianping.com>
2015-10-27 22:37:56 +0800

    fix hardcode url

--- taskfactory-biz/src/main/java/com/mk/taskfactory/common/Constants.java
+++ taskfactory-biz/src/main/java/com/mk/taskfactory/common/Constants.java
@@ -1,11 +1,11 @@
 package com.mk.taskfactory.common;
 
+import com.mk.taskfactory.biz.utils.UrlUtils;
+
 /**
  * Created by Thinkpad on 2015/10/18.
  */
 public class Constants {
-    //public static final String OTS_URL = "http://smlt-ots.imike.cn/ots";
-    public static final String token = "1qaz2wsx";
-    public static final String OTS_URL = "http://ota2test.imike.cn/ots";
-    //public static final String OTS_URL = "http://localhost:9010/ots";
+    public static final String token = UrlUtils.getUrl("ots.token");
+    public static final String OTS_URL = UrlUtils.getUrl("ots.url");
 }
