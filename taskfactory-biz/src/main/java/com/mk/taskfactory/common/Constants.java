8599e9fabbce6b5429a1bb09763b81b58b38db46
huangjie <jie.huang@imike.com>
2015-10-28 18:35:48 +0800

    fix

--- taskfactory-biz/src/main/java/com/mk/taskfactory/common/Constants.java
+++ taskfactory-biz/src/main/java/com/mk/taskfactory/common/Constants.java
@@ -11,6 +11,12 @@ public class Constants {
     public static String token = new PropertiesUtil().getValue("common.properties", "ots.token");
     public static String OTS_URL = new PropertiesUtil().getValue("common.properties", "ots.url");
 
+//    public static final String OTS_URL = "http://smlt-ots.imike.cn/ots";
+//    public static final String token = "1qaz2wsx";
+    //public static final String OTS_URL = "http://ota2test.imike.cn/ots";
+    //public static final String OTS_URL = "http://ots.imike.com/ots";
+    //public static final String OTS_URL = "http://localhost:9010/ots";
+
     public static final Integer PROMO_FININSHED = -1;
 
     public static final Integer PROMOING = 1;
