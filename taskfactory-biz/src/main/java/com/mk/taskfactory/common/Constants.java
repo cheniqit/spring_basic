24328401a54d88faf85e5df422fc34abe196064b
qi.chen <qi.chen@imike.com>
2015-11-02 11:13:56 +0800

    #r merge

--- taskfactory-biz/src/main/java/com/mk/taskfactory/common/Constants.java
+++ taskfactory-biz/src/main/java/com/mk/taskfactory/common/Constants.java
@@ -1,15 +1,12 @@
 package com.mk.taskfactory.common;
 
-import com.mk.taskfactory.biz.utils.PropertiesUtil;
 
-import javax.annotation.Resource;
+import com.mk.taskfactory.biz.utils.PropertiesUtil;
 
 /**
  * Created by Thinkpad on 2015/10/18.
  */
 public class Constants {
-    public static String token = new PropertiesUtil().getValue("common.properties", "ots.token");
-    public static String OTS_URL = new PropertiesUtil().getValue("common.properties", "ots.url");
 
     public static final Integer PROMO_FININSHED = -1;
 
@@ -17,4 +14,8 @@ public class Constants {
 
     public static final Integer PROMO_NOT_START = 0;
 
+    public static String token = new PropertiesUtil().getValue("common.properties", "ots.token");
+    public static String OTS_URL = new PropertiesUtil().getValue("common.properties", "ots.url");
+    //public static final String OTS_URL = "http://ota2test.imike.cn/ots";
+    //public static final String OTS_URL = "http://localhost:9010/ots";
 }
