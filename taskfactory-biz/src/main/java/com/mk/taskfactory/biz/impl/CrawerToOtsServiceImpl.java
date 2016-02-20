ca82b1a8881ccc9456a9edee2eadbb598523e3fa
kangxiaolong <xiaolong.kang@imike.com>
2016-02-20 17:37:08 +0800

    thread 500

--- taskfactory-biz/src/main/java/com/mk/taskfactory/biz/impl/CrawerToOtsServiceImpl.java
+++ taskfactory-biz/src/main/java/com/mk/taskfactory/biz/impl/CrawerToOtsServiceImpl.java
@@ -45,7 +45,7 @@ public class CrawerToOtsServiceImpl implements CrawerToOtsService {
     @Autowired
     private OtsHotelImageService otsHotelImageService;
 
-    private static ExecutorService pool = Executors.newFixedThreadPool(120);
+    private static ExecutorService pool = Executors.newFixedThreadPool(500);
 
 
     public Map<String,Object> commentImg(){
@@ -153,7 +153,7 @@ public class CrawerToOtsServiceImpl implements CrawerToOtsService {
             resultMap.put("SUCCESS", false);
             return resultMap;
         }
-        int pageSize=1000;
+        int pageSize=5000;
         int pageCount=count/pageSize;
         logger.info(String.format("\n====================size={}&pageSize={}&pageCount={}====================\n")
                 ,count,pageSize,pageCount);
