ab56047c967a14456282bd64fade484a60fb5332
kangxiaolong <xiaolong.kang@imike.com>
2016-02-20 18:27:22 +0800

    thread 64

--- taskfactory-biz/src/main/java/com/mk/taskfactory/biz/impl/CrawerToOtsServiceImpl.java
+++ taskfactory-biz/src/main/java/com/mk/taskfactory/biz/impl/CrawerToOtsServiceImpl.java
@@ -45,7 +45,7 @@ public class CrawerToOtsServiceImpl implements CrawerToOtsService {
     @Autowired
     private OtsHotelImageService otsHotelImageService;
 
-    private static ExecutorService pool = Executors.newFixedThreadPool(70);
+    private static ExecutorService pool = Executors.newFixedThreadPool(64);
 
 
     public Map<String,Object> commentImg(){
