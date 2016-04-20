487b2cef5707cfe938f810d0df9e8d75133893aa
huangjie <jie.huang@imike.com>
2016-04-20 18:31:01 +0800

    fix

--- taskfactory-biz/src/main/java/com/mk/taskfactory/model/OrderToCs.java
+++ taskfactory-biz/src/main/java/com/mk/taskfactory/model/OrderToCs.java
@@ -21,6 +21,17 @@ public class OrderToCs {
 
     private String isVaild;
 
+
+    private String result;
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
     public Long getId() {
         return id;
     }
