4f7c6e3d1d23089f005f1dc06db00d302b0a9934
huangjie <jie.huang@imike.com>
2016-04-22 18:36:23 +0800

    fix

--- taskfactory-biz/src/main/java/com/mk/taskfactory/biz/impl/ots/OrderRecommandInfoServiceImpl.java
+++ taskfactory-biz/src/main/java/com/mk/taskfactory/biz/impl/ots/OrderRecommandInfoServiceImpl.java
@@ -1,13 +1,9 @@
 package com.mk.taskfactory.biz.impl.ots;
 
 import com.mk.taskfactory.api.dtos.OrderRecommendInfoDto;
-import com.mk.taskfactory.api.dtos.OrderToCsLogDto;
-import com.mk.taskfactory.api.ots.OrderRecommandInfoService;
-import com.mk.taskfactory.api.ots.OrderToCsLogService;
+import com.mk.taskfactory.api.ots.OrderRecommendInfoService;
 import com.mk.taskfactory.biz.mapper.ots.OrderRecommendInfoMapper;
-import com.mk.taskfactory.biz.mapper.ots.OrderToCsLogMapper;
 import com.mk.taskfactory.model.OrderRecommendInfo;
-import com.mk.taskfactory.model.OrderToCsLog;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,7 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public class OrderRecommandInfoServiceImpl implements OrderRecommandInfoService {
+public class OrderRecommandInfoServiceImpl implements OrderRecommendInfoService {
     @Autowired
     private OrderRecommendInfoMapper mapper;
 
