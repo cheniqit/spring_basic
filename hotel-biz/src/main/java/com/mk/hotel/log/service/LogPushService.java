1a08968442052eeb2b6546be6d39d4acc212078e
huangjie <jie.huang@imike.com>
2016-05-10 11:04:44 +0800

    fix

---
+++ hotel-biz/src/main/java/com/mk/hotel/log/service/LogPushService.java
@@ -0,0 +1,7 @@
+package com.mk.hotel.log.service;
+
+import com.mk.hotel.log.model.LogPush;
+
+public interface LogPushService {
+    public int save(LogPush logPush);
+}
--- hotel-biz/src/main/java/com/mk/hotel/log/service/impl/LogPushServiceImpl.java
+++ hotel-biz/src/main/java/com/mk/hotel/log/service/impl/LogPushServiceImpl.java
@@ -2,13 +2,14 @@ package com.mk.hotel.log.service.impl;
 
 import com.mk.hotel.log.mapper.LogPushMapper;
 import com.mk.hotel.log.model.LogPush;
+import com.mk.hotel.log.service.LogPushService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 
 @Service
-public class LogPushServiceImpl {
+public class LogPushServiceImpl implements LogPushService {
     @Autowired
     private LogPushMapper logPushMapper;
 
