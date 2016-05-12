91b42108e32d8ec7de5fc3ffd044301d5ad07efe
陈奇 <qi.chen@imike.com>
2016-05-12 18:32:19 +0800

    # fix name

--- hotel-biz/src/main/java/com/mk/hotel/remote/amap/AddressInfoRemoteService.java
+++ hotel-biz/src/main/java/com/mk/hotel/remote/amap/AddressInfoRemoteService.java
@@ -12,8 +12,8 @@ import org.apache.commons.lang.StringUtils;
 public class AddressInfoRemoteService {
     public static String REGEO = "/v3/geocode/regeo?output=json&location=%s,%s&key=f8f3fd85e51b484bd18de839f1082429&radius=1000&extensions=all";
 
-    public AddressByLocationResponse findAddressByLocation(String logLocation, String lanLocation){
-        String remoteResult = HttpUtils.getData(Constant.AMAP_REMOTE_URL + String.format(this.REGEO, logLocation, lanLocation));
+    public AddressByLocationResponse findAddressByLocation(String latLocation, String longLocation){
+        String remoteResult = HttpUtils.getData(Constant.AMAP_REMOTE_URL + String.format(this.REGEO, latLocation, longLocation));
         if(StringUtils.isBlank(remoteResult)){
             return null;
         }
--- hotel-biz/src/test/java/com/mk/hotel/remote/amap/AddressInfoRemoteServiceTest.java
+++ hotel-biz/src/test/java/com/mk/hotel/remote/amap/AddressInfoRemoteServiceTest.java
@@ -1,6 +1,5 @@
 package com.mk.hotel.remote.amap;
 
-import com.mk.framework.JsonUtils;
 import com.mk.hotel.remote.amap.json.AddressByLocationResponse;
 import org.junit.Assert;
 import org.junit.Test;
@@ -13,9 +12,9 @@ public class AddressInfoRemoteServiceTest {
 
     @Test
     public void testFindAddressByLocation() throws Exception {
-        String logLocation = "106.57329600";
-        String lanLocation = "29.55476000";
-        AddressByLocationResponse addressByLocationResponse = addressInfoRemoteService.findAddressByLocation(logLocation, lanLocation);
+        String latLocation = "106.57329600";
+        String longLocation = "29.55476000";
+        AddressByLocationResponse addressByLocationResponse = addressInfoRemoteService.findAddressByLocation(latLocation, longLocation);
         Assert.assertNotNull(addressByLocationResponse);
         Assert.assertNotNull(addressByLocationResponse.getInfocode());
         Assert.assertEquals("OK", addressByLocationResponse.getInfo());
