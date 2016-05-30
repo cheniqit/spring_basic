2bde648bfcd50d841704c880e2d7ef0aba2d8c58
陈奇 <qi.chen@imike.com>
2016-05-30 17:25:02 +0800

    #r add file upload

--- hotel-biz/src/main/java/com/mk/hotel/common/Constant.java
+++ hotel-biz/src/main/java/com/mk/hotel/common/Constant.java
@@ -6,6 +6,7 @@ import com.mk.framework.PropertiesUtil;
  * Created by chenqi on 16/5/30.
  */
 public class Constant {
+    public static final String UPLOAD_PATH = "upload/";
 
     /**七牛文件下载路径key*/
     public static final String QINIU_DOWNLOAD_ADDRESS = new PropertiesUtil().getValue("common.properties", "qiniuDownloadAddress");
@@ -20,5 +21,4 @@ public class Constant {
     /**七牛空间名称key*/
     public static final String QINIU_BUCKET = new PropertiesUtil().getValue("common.properties", "qiniuBucket");
     public static final String QINIU_RESOURCE_BUCKET=new PropertiesUtil().getValue("common.properties", "resourceBucket");
-    public static final String HOTEL_REMOTE_URL = new PropertiesUtil().getValue("common.properties", "hotel.remote.url");
 }
--- hotel-web/src/main/java/com/mk/hotel/base/controller/FileController.java
+++ hotel-web/src/main/java/com/mk/hotel/base/controller/FileController.java
@@ -1,8 +1,11 @@
 package com.mk.hotel.base.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.mk.framework.FileUpload;
+import com.mk.hotel.common.Constant;
 import com.mk.hotel.common.utils.QiniuUtils;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,31 +14,46 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
+import java.util.UUID;
+
 /**
  * Created by chenqi on 16/5/30.
  */
 @RestController
 @RequestMapping(value = "/file", method = RequestMethod.POST)
 public class FileController {
-    private static String UPLOAD_PATH = "";
     Logger logger = LoggerFactory.getLogger(FileController.class);
 
     @RequestMapping("/upload")
-    public void upload(String fileName, HttpServletRequest request) throws IOException{
+    public JSONObject upload(String fileName, HttpServletRequest request) throws IOException{
+        JSONObject jsonObj = new JSONObject();
         try {
-            FileUpload.uploadFile(request, UPLOAD_PATH, fileName);
+            FileUpload.uploadFile(request, Constant.UPLOAD_PATH, fileName);
         }catch (Exception e){
             throw new IOException("文件上传失败");
         }
         String rootPath = request.getSession().getServletContext().getRealPath("/");
-        File rmFile = new File(rootPath + UPLOAD_PATH + fileName);
+        File rmFile = new File(rootPath + Constant.UPLOAD_PATH + fileName);
         if (!rmFile.exists()) {
             throw new IOException("文件上传失败");
         }
-        //QiniuUtils.uploadAndTry(rmFile.get)
+        String qiNiuFileName = UUID.randomUUID().toString()+ ".jpg";
+        InputStream inputStream = null;
+        try {
+            inputStream = new FileInputStream(rmFile);
+            QiniuUtils.uploadAndTry(IOUtils.toByteArray(inputStream), qiNiuFileName, Constant.QINIU_BUCKET);
+        }catch (Exception e){
+            jsonObj.put("success", "F");
+            jsonObj.put("errmsg", "文件上传失败,图片服务器异常");
+            return jsonObj;
+        }finally {
+            IOUtils.closeQuietly(inputStream);
+        }
+        String qiNiuUrl = Constant.QINIU_DOWNLOAD_ADDRESS+"/"+qiNiuFileName;
+        jsonObj.put("success", "T");
+        jsonObj.put("url", qiNiuUrl);
+        return jsonObj;
     }
 
     @RequestMapping("/download")
@@ -47,7 +65,7 @@ public class FileController {
             response.setContentType("image/jpeg; charset=utf-8");
             File file = null;
             try {
-                FileUpload.getFile(fileName);
+                file = FileUpload.getFile(fileName);
             } catch (Exception e) {
                 e.printStackTrace();
                 throw new IOException(e.getMessage());
@@ -55,9 +73,7 @@ public class FileController {
             os.write(FileUtils.readFileToByteArray(file));
             os.flush();
         } finally {
-            if (os != null) {
-                os.close();
-            }
+           IOUtils.closeQuietly(os);
         }
     }
 }
