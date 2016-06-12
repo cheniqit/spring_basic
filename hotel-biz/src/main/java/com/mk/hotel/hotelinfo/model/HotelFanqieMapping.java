dfc03b0e9bfe8c466f566841794704bce07879b6
huangjie <jie.huang@imike.com>
2016-06-12 10:41:48 +0800

    add fanqie mapping

---
+++ hotel-biz/src/main/java/com/mk/hotel/hotelinfo/model/HotelFanqieMapping.java
@@ -0,0 +1,95 @@
+package com.mk.hotel.hotelinfo.model;
+
+import java.util.Date;
+
+public class HotelFanqieMapping {
+    private Long id;
+
+    private Long hotelId;
+
+    private Integer pattern;
+
+    private Long accountId;
+
+    private Date createDate;
+
+    private String createBy;
+
+    private Date updateDate;
+
+    private String updateBy;
+
+    private String isValid;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getHotelId() {
+        return hotelId;
+    }
+
+    public void setHotelId(Long hotelId) {
+        this.hotelId = hotelId;
+    }
+
+    public Integer getPattern() {
+        return pattern;
+    }
+
+    public void setPattern(Integer pattern) {
+        this.pattern = pattern;
+    }
+
+    public Long getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(Long accountId) {
+        this.accountId = accountId;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(String createBy) {
+        this.createBy = createBy == null ? null : createBy.trim();
+    }
+
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+    }
+
+    public String getUpdateBy() {
+        return updateBy;
+    }
+
+    public void setUpdateBy(String updateBy) {
+        this.updateBy = updateBy == null ? null : updateBy.trim();
+    }
+
+    public String getIsValid() {
+        return isValid;
+    }
+
+    public void setIsValid(String isValid) {
+        this.isValid = isValid == null ? null : isValid.trim();
+    }
+}
