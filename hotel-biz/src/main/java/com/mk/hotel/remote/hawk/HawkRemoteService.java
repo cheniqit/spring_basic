84040c5cbdb7204f4eba25fe79e0a35afcded7c3
陈奇 <qi.chen@imike.com>
2016-06-28 14:33:59 +0800

    Signed-off-by: 陈奇 <qi.chen@imike.com>

--- hotel-biz/src/main/java/com/mk/hotel/remote/hawk/HawkRemoteService.java
+++ hotel-biz/src/main/java/com/mk/hotel/remote/hawk/HawkRemoteService.java
@@ -3,6 +3,7 @@ package com.mk.hotel.remote.hawk;
 import com.mk.framework.HttpUtils;
 import com.mk.framework.JsonUtils;
 import com.mk.framework.UrlUtils;
+import com.mk.framework.excepiton.MyException;
 import com.mk.hotel.remote.fanqielaile.hotel.json.inn.InnList;
 import org.apache.commons.collections.map.HashedMap;
 import org.apache.commons.lang.StringUtils;
@@ -23,6 +24,10 @@ public class HawkRemoteService {
 
     private final String HOTEL_OFFLINE = "/hotelcallback/hoteloffline";
     private final String ROOMTYPE_OFFLINE = "/hotelcallback/roomtypeoffline";
+    private final String ORDER_NOTIFY = "/order/orderNotify";
+    private final String ORDER_NOTIFY_1_MINUTE_JOB = "/order/orderNotify1MinuteJob";
+    private final String ORDER_NOTIFY_5_MINUTE_JOB = "/order/orderNotify5MinuteJob";
+    private final String ORDER_NOTIFY_15_MINUTE_JOB = "/order/orderNotify15MinuteJob";
 
     private Logger logger = LoggerFactory.getLogger(HawkRemoteService.class);
 
@@ -64,4 +69,51 @@ public class HawkRemoteService {
 
         return remoteResult;
     }
+
+
+    public String orderNotify(String orderNo, Integer orderStatus) {
+        if (StringUtils.isBlank(orderNo)) {
+            throw new MyException("orderNo参数错误");
+        }
+        if (orderStatus == null) {
+            throw new MyException("orderStatus参数错误");
+        }
+
+        StringBuilder url = new StringBuilder()
+                .append(DOMAIN)
+                .append(ORDER_NOTIFY);
+
+        Map<String, String> param = new HashMap();
+        param.put("orderNo", orderNo);
+        param.put("orderStatus", orderStatus.toString());
+        String remoteResult = HttpUtils.doPost(url.toString(), param);
+        return remoteResult;
+    }
+
+    public String orderNotify1MinuteJob() {
+        StringBuilder url = new StringBuilder()
+                .append(DOMAIN)
+                .append(ORDER_NOTIFY_1_MINUTE_JOB);
+        Map<String, String> param = new HashMap();
+        String remoteResult = HttpUtils.doPost(url.toString(), param);
+        return remoteResult;
+    }
+    public String orderNotify5MinuteJob() {
+        StringBuilder url = new StringBuilder()
+                .append(DOMAIN)
+                .append(ORDER_NOTIFY_5_MINUTE_JOB);
+        Map<String, String> param = new HashMap();
+        String remoteResult = HttpUtils.doPost(url.toString(), param);
+        return remoteResult;
+    }
+
+    public String orderNotify15MinuteJob() {
+        StringBuilder url = new StringBuilder()
+                .append(DOMAIN)
+                .append(ORDER_NOTIFY_15_MINUTE_JOB);
+        Map<String, String> param = new HashMap();
+        String remoteResult = HttpUtils.doPost(url.toString(), param);
+        return remoteResult;
+    }
+
 }
