db17416ade2bd5aa6781472ddae9bc380984ea44
huangjie <jie.huang@imike.com>
2016-10-09 18:58:32 +0800

    add queryStates

--- hotel-biz/src/main/java/com/mk/hotel/roomstates/service/impl/RoomStatesServiceImpl.java
+++ hotel-biz/src/main/java/com/mk/hotel/roomstates/service/impl/RoomStatesServiceImpl.java
@@ -1,16 +1,19 @@
 package com.mk.hotel.roomstates.service.impl;
 
+import com.mk.framework.DateUtils;
 import com.mk.hotel.common.Constant;
 import com.mk.hotel.message.MsgProducer;
 import com.mk.hotel.roomstates.IRoomStatesService;
 import com.mk.hotel.roomstates.dto.RoomStatesDto;
 import com.mk.hotel.roomtype.RoomTypePriceService;
+import com.mk.hotel.roomtype.RoomTypeStockService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -20,7 +23,6 @@ import java.util.List;
 @Service
 public class RoomStatesServiceImpl implements IRoomStatesService {
 
-
     private static Logger logger = LoggerFactory.getLogger(RoomStatesServiceImpl.class);
 
     @Autowired
@@ -29,11 +31,28 @@ public class RoomStatesServiceImpl implements IRoomStatesService {
     @Autowired
     private RoomTypePriceService roomTypePriceService;
 
+    @Autowired
+    private RoomTypeStockService roomTypeStockService;
+
     @Override
     public List<RoomStatesDto> queryStates(Long roomTypeId, Date startDate, Date endDate) {
+        List<RoomStatesDto> roomStatesDtoList = new ArrayList<RoomStatesDto>();
+        if (null == roomTypeId || null == startDate) {
+            return roomStatesDtoList;
+        }
 
+        if (null == endDate) {
+            endDate = DateUtils.addDays(startDate, 30);
+        }
 
-        return null;
+        //
+        Date[] dates = DateUtils.getStartEndDate(startDate, endDate);
+        for (Date date : dates) {
+            RoomStatesDto dto = new RoomStatesDto();
+            roomStatesDtoList.add(dto);
+        }
+
+        return roomStatesDtoList;
     }
 
     @Override
