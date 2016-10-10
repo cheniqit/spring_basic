c26f127b5438690170a867385567da8074fd8313
huangjie <jie.huang@imike.com>
2016-10-10 10:58:27 +0800

    add queryStates

--- hotel-api/src/main/java/com/mk/hotel/roomstates/IRoomStatesService.java
+++ hotel-api/src/main/java/com/mk/hotel/roomstates/IRoomStatesService.java
@@ -11,15 +11,15 @@ import java.util.List;
  */
 public interface IRoomStatesService {
 
-    List<RoomStatesDto> queryStates(Long roomTypeId, Date startDate, Date endDate);
+    List<RoomStatesDto> queryStates(Long roomTypeId, Date startDate, Date endDate, String token);
 
     int updatePriceAndStock(Long roomTypeId, Date startDate, Date endDate,
                             BigDecimal marketPrice, BigDecimal salePrice, BigDecimal settlePrice,
-                            BigDecimal totalStock);
+                            BigDecimal totalStock, String optionId, String token);
 
     int updatePrice(Long roomTypeId, Date startDate, Date endDate,
-                                    BigDecimal marketPrice, BigDecimal salePrice, BigDecimal settlePrice);
+                                    BigDecimal marketPrice, BigDecimal salePrice, BigDecimal settlePrice, String optionId, String token);
 
     int updateStock(Long roomTypeId, Date startDate, Date endDate,
-                                    BigDecimal totalStock);
+                                    BigDecimal totalStock, String optionId, String token);
 }
--- hotel-biz/src/main/java/com/mk/hotel/roomstates/service/impl/RoomStatesServiceImpl.java
+++ hotel-biz/src/main/java/com/mk/hotel/roomstates/service/impl/RoomStatesServiceImpl.java
@@ -1,12 +1,15 @@
 package com.mk.hotel.roomstates.service.impl;
 
 import com.mk.framework.DateUtils;
+import com.mk.framework.excepiton.MyException;
+import com.mk.framework.security.MD5;
 import com.mk.hotel.common.Constant;
 import com.mk.hotel.message.MsgProducer;
 import com.mk.hotel.roomstates.IRoomStatesService;
 import com.mk.hotel.roomstates.dto.RoomStatesDto;
 import com.mk.hotel.roomtype.RoomTypePriceService;
 import com.mk.hotel.roomtype.RoomTypeStockService;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,7 +38,10 @@ public class RoomStatesServiceImpl implements IRoomStatesService {
     private RoomTypeStockService roomTypeStockService;
 
     @Override
-    public List<RoomStatesDto> queryStates(Long roomTypeId, Date startDate, Date endDate) {
+    public List<RoomStatesDto> queryStates(Long roomTypeId, Date startDate, Date endDate, String token) {
+        //
+        this.checkToken(roomTypeId, token);
+        //
         List<RoomStatesDto> roomStatesDtoList = new ArrayList<RoomStatesDto>();
         if (null == roomTypeId || null == startDate) {
             return roomStatesDtoList;
@@ -49,6 +55,7 @@ public class RoomStatesServiceImpl implements IRoomStatesService {
         Date[] dates = DateUtils.getStartEndDate(startDate, endDate);
         for (Date date : dates) {
             RoomStatesDto dto = new RoomStatesDto();
+
             roomStatesDtoList.add(dto);
         }
 
@@ -56,23 +63,32 @@ public class RoomStatesServiceImpl implements IRoomStatesService {
     }
 
     @Override
-    public int updatePriceAndStock(Long roomTypeId, Date startDate, Date endDate, BigDecimal marketPrice, BigDecimal salePrice, BigDecimal settlePrice, BigDecimal totalStock) {
+    public int updatePriceAndStock(Long roomTypeId, Date startDate, Date endDate,
+                                   BigDecimal marketPrice, BigDecimal salePrice, BigDecimal settlePrice,
+                                   BigDecimal totalStock, String optionId, String token) {
+        //
+        this.checkToken(roomTypeId, token);
 
+        //
         int i = 0;
         if (null != settlePrice) {
-            i += this.updatePrice(roomTypeId, startDate, endDate, marketPrice, salePrice, settlePrice);
+            i += this.updatePrice(roomTypeId, startDate, endDate, marketPrice, salePrice, settlePrice, optionId, token);
         }
 
         if (null != totalStock) {
-            i += this.updateStock(roomTypeId, startDate, endDate, totalStock);
+            i += this.updateStock(roomTypeId, startDate, endDate, totalStock, optionId, token);
         }
 
         return i;
     }
 
     @Override
-    public int updatePrice(Long roomTypeId, Date startDate, Date endDate, BigDecimal marketPrice, BigDecimal salePrice, BigDecimal settlePrice) {
+    public int updatePrice(Long roomTypeId, Date startDate, Date endDate, BigDecimal marketPrice, BigDecimal salePrice, BigDecimal settlePrice, String optionId, String token) {
 
+        //
+        this.checkToken(roomTypeId, token);
+
+        //
         //TODO push message
         msgProducer.sendMsg(Constant.TOPIC_ROOMTYPE_PRICE, "test");
 
@@ -80,9 +96,33 @@ public class RoomStatesServiceImpl implements IRoomStatesService {
     }
 
     @Override
-    public int updateStock(Long roomTypeId, Date startDate, Date endDate, BigDecimal totalStock) {
+    public int updateStock(Long roomTypeId, Date startDate, Date endDate, BigDecimal totalStock, String optionId, String token) {
+
+        //
+        this.checkToken(roomTypeId, token);
 
         //TODO push message
         return 0;
+    }
+
+    /**
+     * 认证
+     * @param token
+     * @return
+     */
+    private boolean checkToken(Long roomTypeId, String token) {
+        if (null == roomTypeId || StringUtils.isBlank(token)) {
+            throw new MyException("认证错误");
+        }
+
+        //
+        String origin = roomTypeId + "{boss}";
+        String security = MD5.MD5Encode(origin);
+
+        if (security.equals(token)) {
+            return true;
+        } else {
+            throw new MyException("认证错误");
+        }
     }
 }
