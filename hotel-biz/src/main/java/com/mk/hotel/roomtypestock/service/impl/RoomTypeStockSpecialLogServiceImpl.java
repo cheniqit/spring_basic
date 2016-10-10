dbbbf8b565aee311cc08ab2b3a0db2714595732c
huangjie <jie.huang@imike.com>
2016-10-10 14:16:28 +0800

    fix

--- hotel-biz/src/main/java/com/mk/hotel/roomtypestock/service/impl/RoomTypeStockSpecialLogServiceImpl.java
+++ hotel-biz/src/main/java/com/mk/hotel/roomtypestock/service/impl/RoomTypeStockSpecialLogServiceImpl.java
@@ -2,12 +2,14 @@ package com.mk.hotel.roomtypestock.service.impl;
 
 import com.mk.hotel.roomtypestock.RoomTypeStockSpecialLogService;
 import com.mk.hotel.roomtypestock.dto.RoomTypeStockSpecialLogDto;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 /**
  * Created by yanbaobin on 2016/10/10 9:44.
  */
+@Service
 public class RoomTypeStockSpecialLogServiceImpl implements RoomTypeStockSpecialLogService {
 
 	@Override
